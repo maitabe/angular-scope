94ddefce56fc1d77349f2963444278c0ea15ec0b
maitabe <martushb@gmail.com>
2016-11-28 14:33:58 +0200

    initial commit

---
+++ OuterController.js
@@ -0,0 +1,67 @@
+
+
+// app.controller('OuterController', function($scope) {
+// 	$scope.outer = 'hello';
+// });
+
+// app.controller('InnerController', function($scope) {
+// 	$scope.inner = "bonjour";
+// });
+
+
+// app.controller('EditController', function($scope) {
+// 	// $scope.message = "hola";
+// 	$scope.userIsAdmin = true;
+// 	// $scope.isMember = true;
+
+// 	$scope.message  = {
+// 		content: 'redact me'
+// 	};
+// });
+
+// app.controller('Controller1', function($scope) {
+// 	// $scope.message = "hola";
+// 	$scope.value1 =  'im controller 1';
+// });
+
+
+// app.controller('Controller2', function($scope) {
+// 	// $scope.message = "hola";
+// 	// $scope.BigValue =  value2.message;
+// 	$scope.value2 = {
+// 		message: 'im 2'
+// 	}
+// 	$scope.BigValue = ' imback to 2';
+// });
+
+// app.controller('Controller3', function($scope) {
+// 	// $scope.message = "hola";
+// 	$scope.BigValue =  'lets try';
+// });
+
+// app.controller('Controller4', function($scope) {
+// 	// $scope.message = "hola"
+// });
+
+// app.controller('Controller5', function($scope) {
+// 	// $scope.newValue =  BigValue;
+// });
+
+// app.controller('Controller6', function($scope) {
+
+// });
+
+
+
+
+app.controller('MainCtrl', function () {
+  this.title = 'MainCtrl title';
+});
+
+app.controller('AnotherCtrl', function () {
+  this.title = 'AnotherCtrl  title';
+});
+
+app.controller('YetAnotherCtrl', function () {
+  this.title = 'YetAnotherCtrl title';
+});
